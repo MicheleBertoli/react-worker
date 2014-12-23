8cd04e98f7f432c3bd58e16603dda9ab06f4bf34
Sandro Paganotti <sandro.paganotti@gmail.com>
2014-12-23 21:39:29 +0000

    caching rendered pages until next SW update

--- public/worker.js
+++ public/worker.js
@@ -1,4 +1,14 @@
+/*global caches self fetch Router Routes Response React Url*/
+
+importScripts('serviceworker-cache-polyfill.js');
 importScripts('build.js');
+
+var pagesCache = 'pages';
+var options = {
+  headers: {
+    'Content-Type': 'text/html'
+  }
+};
 
 self.addEventListener('fetch', function(event) {
   if (/\.js$/.test(event.request.url)) {
@@ -8,11 +18,18 @@ self.addEventListener('fetch', function(event) {
   }
 });
 
-var options = {
-  headers: {
-    'Content-Type': 'text/html'
-  }
-};
+// we use activate here since we want to perform a full clean
+// each time build.js is updated, and this happens during SW 
+// update -- in the future we might use caches.delete -- not yet implemented
+self.addEventListener('activate', function(event) {
+  caches.open(pagesCache).then(function(cache){
+    cache.keys().then(function(requests) {
+      requests.forEach(function(request) {
+        cache.delete(request);
+      });
+    });      
+  });
+});
 
 function _static(event) {
   event.respondWith(
@@ -21,10 +38,25 @@ function _static(event) {
 }
 
 function _app(event) {
-  Router.run(Routes, _path(event.request.url), function(Handler) {
-    var html = _render(Handler);
-    var response = new Response('<!DOCTYPE html>' + html, options);
-    event.respondWith(response);
+  event.respondWith(
+    caches.match(event.request).then(function(page){
+      return page || new Promise(function(resolve, reject){
+        Router.run(Routes, _path(event.request.url), function(Handler) {
+          var html = _render(Handler);
+          var response = new Response('<!DOCTYPE html>' + html, options);
+          resolve(storeResponse(pagesCache, event.request, response));
+        }); 
+      });
+    })
+  );
+}
+
+function storeResponse(cacheName, request, response){
+  return caches.open(cacheName).then(function(cache){
+    return cache.put(request, response.clone()).then(function(){
+      console.log('stored');
+      return response;
+    });
   });
 }
 
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(process.env.PORT || 3000, function() {
+var server = app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0', function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('App listening at http://%s:%s', host, port);
