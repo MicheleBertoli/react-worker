dd549445cdc75e1b664d2093a80604e02ab7d94e
Michele Bertoli <michele@berto.li>
2014-12-26 09:39:56 +0000

    extracted route function

--- public/worker.js
+++ public/worker.js
@@ -1,68 +1,67 @@
-/*global caches self fetch Router Routes Response React Url*/
-
 importScripts('serviceworker-cache-polyfill.js');
 importScripts('build.js');
 
-var pagesCache = 'pages';
+var cacheKey = 'pages';
 var options = {
-    headers: {
-        'Content-Type': 'text/html'
-    }
+  headers: {
+    'Content-Type': 'text/html'
+  }
 };
 
 self.addEventListener('fetch', function(event) {
-    if (/\.js$/.test(event.request.url)) {
-        _static(event);
-    } else {
-        _app(event);
-    }
+  if (/\.js$/.test(event.request.url)) {
+    _static(event);
+  } else {
+    _app(event);
+  }
 });
 
-// we use activate here since we want to perform a full clean
-// each time build.js is updated, and this happens during SW 
-// update -- in the future we might use caches.delete -- not yet implemented
 self.addEventListener('activate', function(event) {
-    caches.open(pagesCache).then(function(cache) {
-        cache.keys().then(function(requests) {
-            requests.forEach(function(request) {
-                cache.delete(request);
-            });
-        });
+  caches.open(cacheKey).then(function(cache) {
+    cache.keys().then(function(requests) {
+      requests.forEach(function(request) {
+        cache.delete(request);
+      });
     });
+  });
 });
 
 function _static(event) {
-    event.respondWith(
-        fetch(event.request.url)
-    );
+  event.respondWith(
+    fetch(event.request.url)
+  );
 }
 
 function _app(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(page) {
-            return page || new Promise(function(resolve, reject) {
-                Router.run(Routes, _path(event.request.url), function(Handler) {
-                    var html = _render(Handler);
-                    var response = new Response('<!DOCTYPE html>' + html, options);
-                    _storeResponse(pagesCache, event.request, response);
-                    resolve(response);
-                });
-            });
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(function(page) {
+      return page || _route(event)
+    })
+  );
 }
 
-function _storeResponse(cacheName, request, response) {
-    return caches.open(cacheName).then(function(cache) {
-        return cache.put(request, response.clone());
+function _route(event) {
+  return new Promise(function(resolve, reject) {
+    Router.run(Routes, _path(event.request.url), function(Handler) {
+      var html = _render(Handler);
+      var response = new Response('<!DOCTYPE html>' + html, options);
+      _store(event.request, response);
+      resolve(response);
     });
-}
-
-function _render(Handler) {
-    var handler = React.createFactory(Handler);
-    return React.renderToString(handler());
+  });
 }
 
 function _path(url) {
-    return Url.parse(url).path;
+  return Url.parse(url).path;
+}
+
+function _render(Handler) {
+  var handler = React.createFactory(Handler);
+  return React.renderToString(handler());
+}
+
+function _store(request, response) {
+  return caches.open(cacheKey).then(function(cache) {
+    return cache.put(request, response.clone());
+  });
 }
